78ba604ee1a66375f6412c22ada108d9f4a6d4f6
butchland <butchland@d2586e82-b24c-11de-b8dd-ab4b6ce6d521>
2009-10-24 18:03:47 +0000

    fixed bug in page where the job instance did not display - due to a bug in the gsp code and added tests that the checked that the values were retrieved from the db

--- test/integration/com/orangeandbronze/batchmon/JobNameControllerTests.groovy
+++ test/integration/com/orangeandbronze/batchmon/JobNameControllerTests.groovy
@@ -12,19 +12,29 @@ class JobNameControllerTests extends ControllerUnitTestCase {
     protected void tearDown() {
         super.tearDown()
     }
+    
     void testServiceSetup() {
     	assertNotNull jobInstanceService
     }
-    
+
     void testControllerSetup() {
     	assertNotNull controller
     	assertNotNull controller.jobInstanceService
     }
+    
 
     void testListJobNames() {
     	def model = controller.index.call()
     	assertNotNull model
     	assertNotNull model.jobNames
     	assertEquals 1, model.jobNamesTotal
+    }
+    void testShowJobExecution() {
+    	controller.params.id = 44
+    	def model = controller.showJobExecution.call()
+    	assertNotNull model.jobExecution
+    	assertNotNull model.jobExecution.jobInstance
+    	assertEquals model.jobExecution.jobInstance, model.jobInstance
+    	assertEquals 39, model.jobInstance.id
     }
 }
